c0df0b2c8509470a7bb51ed272987c86ce613293
Roman <roma.bruhanchik@gmail.com>
2023-04-25 20:07:22 +0400

    Initial commit

---
+++ src/main/java/org/virtushawk/apachekafkaplayground/ApacheKafkaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package org.virtushawk.apachekafkaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheKafkaPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/virtushawk/apachekafkaplayground/ApacheKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.virtushawk.apachekafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
